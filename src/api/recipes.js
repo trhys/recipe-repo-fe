0b18a9b5a63cd624ef080d59176445c4b66c86a3
trhys <treese1993@gmail.com>
2026-05-12 15:51:25 +0000

    finished recipe creator

--- src/api/recipes.js
+++ src/api/recipes.js
@@ -23,18 +23,39 @@ export function useGetIngredients() {
 }
 
 // Get ingredient units for recipe creator
-export async function useGetUnits(id) {
+export function useGetUnits(id) {
+	const { data, error, isLoading } = useSWR(`${API_BASE}/api/ingredients/${id}/units`, fetcher)
+	return { data, error, isLoading }
+}
+
+// Create new recipe and return success status + recipe id if successful
+export async function postRecipe(title, image, ingredients, description, instructions) {
 	try {
-		const response = await fetch(`${API_BASE}/api/ingredients`, {
+		const ingData = ingredients.map((i) => ({
+			id: i.id,
+			quantity: parseFloat(i.quantity) || 0,
+			unit: i.units,
+		}));
+
+		const body = JSON.stringify({ title: title, ingredients: ingData, description: description, instructions: instructions })
+
+		const formData = new FormData()
+		formData.append("payload", body)
+		formData.append("image", image)
+
+		let response = await fetch(`${API_BASE}/api/recipes`, {
 			method: "POST",
-			body: JSON.stringify({ ingredient_id: id }),
+			credentials: 'include',
+			body: formData,
 		})
 
 		const data = await response.json()
-		if (!response.ok) throw new Error(`Couldn't fetch units - ERROR: ${data.error}`)
-
-		return data
+		
+		if (response.ok) {
+			return { id: data.id, ok: true, message: null }
+		}
 	} catch (error) {
 		console.log(error)
+		return { id: null, ok: false, message: error }
 	}
-}
+}	
--- src/pages/create.jsx
+++ src/pages/create.jsx
@@ -1,64 +1,121 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router'
 import { useAuth } from '../components/auth.jsx'
-import { useGetIngredients, useGetUnits } from '../api/recipes.js'
+import { useGetIngredients, useGetUnits, postRecipe } from '../api/recipes.js'
 import './create.css'
+
+function UnitSelect({ ingredientId, selectedUnit, onSelect }) {
+    	const { data, error, isLoading } = useGetUnits(ingredientId);
+	if (error) console.log(error)
+
+    return (
+        <select 
+	    value={selectedUnit} 
+	    onChange={(e) => onSelect(e.target.value)}
+	    disabled={isLoading || !ingredientId}>
+
+            <option value="">{!ingredientId ? '...' : 'Select units'}</option>
+	    {data?.units.map((opt) => (
+                <option key={opt.name} value={opt.name}>
+                    {opt.name}
+                </option>
+            ))}
+
+        </select>
+    );
+}
 
 export function RecipeCreator() {
 	const { user, logout } = useAuth();
+	const navigate = useNavigate();
+
+	/* STATE */
 	const [title, setTitle] = useState('')
 	const [image, setImage] = useState(null)
+	const [preview, setPreview] = useState(null)
 	const [ingredients, setIngredients] = useState([{ rowID: Date.now(), id: '', quantity: 0, units: '' }])
 	const [description, setDescription] = useState('')
 	const [instructions, setInstructions] = useState('')
-    const [availableUnits, setAvailableUnits] = useState([{ row: '', units: [] }])
+    	
+	/* Get all ingredients from backend to populate select options */
 	const { data: ingredientData, error: ingredientError, isLoading: ingredientIsLoading } = useGetIngredients()
 
 	if (ingredientError) return (
 		<p>Something went wrong</p>
 	)
 
-	function handleSubmit() {}
+	async function handleSubmit(e) {
+		e.preventDefault()
+		let { id, ok, message } = await postRecipe(title, image, ingredients, description, instructions)
+		if (ok) {
+			navigate(`/recipes/${id}`)
+		} else alert(`Failed! ${message}`)
+	}
+
 	function handleSelectIngredient(rowID, id) {
-        let units = postGetUnits(id)
-		setIngredients(ingredients.map(row => row.rowID === rowID ? {...row, id: id } : row));
-        //setAvailableUnits(availableUnits.map(row => { row: id, units: units })
+		setIngredients(ingredients.map(row => row.rowID === rowID ? {...row, id: id } : row))
 	}
 
 	function handleSelectQuantity(rowID, value) {
-		setIngredients(ingredients.map(row => row.rowID === rowID ? {...row, quantity: value } : row));
+		setIngredients(ingredients.map(row => row.rowID === rowID ? {...row, quantity: value } : row))
 	}
 
-    function handleSelectUnits(rowID, unit) {
-        setIngredients(ingredients.map(row => row.rowID === rowID ? {...row, units: unit } : row));
-    }
+    	function handleSelectUnits(rowID, unit) {
+        	setIngredients(ingredients.map(row => row.rowID === rowID ? {...row, units: unit } : row))
+    	}
+
+	const handleSelectImage = (e) => {
+		const file = e.target.files[0]
+		if (file) {
+			setImage(file)
+			setPreview(URL.createObjectURL(file))
+		}
+	}
 
 	const addRow = (e) => {
 		e.preventDefault()
-		setIngredients([...ingredients, { rowID: Date.now(), id: '' }]);
+		let rowID = Date.now()
+		setIngredients([...ingredients, { rowID: rowID, id: '', quantity: 0, units: '' }])
 	}
 
 	const removeRow = (rowID) => {
-	    if (ingredients.length > 1) setIngredients(ingredients.filter((row) => row.rowID !== rowID));	
-	};
+	    if (ingredients.length > 1) {
+		    setIngredients(ingredients.filter((row) => row.rowID !== rowID))
+	    }
+	}
 
 	return (
 		<>
 		<form onSubmit={handleSubmit}>
 		<input
+		  className="recipe-title"
 		  type="text"
 		  value={title}
 		  placeholder="Title"
 		  onChange={e => setTitle(e.target.value)}
 		  required
 		/>
-		<div>
-		<label>Upload image:</label>
-		<input
-		  type="file"
-		  value={image}
-		  onChange={e => setImage(e.target.value)}
-		  accept=".jpg, .jpeg, .png"
-		/></div>
+		<div className="image-upload">
+		    <label className="image-upload-label">
+		      {preview ? (
+			    <div className="preview-container">
+				<img src={preview} alt="Preview" className="image-preview" />
+				<div className="change-image-overlay">Change Image</div>
+			    </div>
+			) : (
+			    <>
+				<strong>📷 Click to upload cover image</strong>
+				<span>JPG, PNG or JPEG</span>
+			    </>
+			)}
+		      <input
+			type="file"
+			className="hidden-file-input"
+			onChange={handleSelectImage}
+			accept=".jpg, .jpeg, .png"
+		      />
+		    </label>
+		  </div>
 		<div className="ingredient-select">
 			<div className="ingredients">
 				<label>Ingredients</label><hr />
@@ -78,7 +135,6 @@ export function RecipeCreator() {
 							))
 						}
 						</select>
-						<label>Quantity:</label>
 						<input
 							type="number"
 							value={row.quantity}
@@ -86,21 +142,40 @@ export function RecipeCreator() {
 							onChange={e => handleSelectQuantity(row.rowID, e.target.value)}
 							required
 						/>
-                        <label>Units:</label>
-                        <select
-                        value={row.units}
-                        onChange={e => handleSelectUnits(row.rowID, e.target.value)}
-                        >
-                        <option value="">{row.id == "" ? '...' : 'Select units'}</option>
-                        {}
-                          </select>
-						<button type="button" onClick={() => removeRow(row.rowID)}>x</button>
-					</div>
+				
+				<UnitSelect
+					key={`${row.rowID}-${row.id}`}
+					ingredientId={row.id}
+					selectedUnit={row.units}
+					onSelect={(unit) => handleSelectUnits(row.rowID, unit)}
+				/>
+
+				<button type="button" onClick={() => removeRow(row.rowID)}>x</button>
+				</div>
+				
 				))}
 
 				<button type="button" onClick={addRow}>Add</button>
+				<hr />
 			</div>
 		</div>
+
+		<label>Description:</label>
+		<textarea 
+		    placeholder="Description" 
+		    value={description} 
+		    onChange={e => setDescription(e.target.value)} 
+		/><br/>
+		
+		<label>Instructions:</label>
+		<textarea 
+		    placeholder="Instructions" 
+		    value={instructions} 
+		    onChange={e => setInstructions(e.target.value)} 
+		/><br/>
+
+		<button type="submit">Create Recipe</button>
+
 		</form>
 		</>
 	)
